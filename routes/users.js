91e116b3ad41ad42e630bf7b9c822abf5fc8b46b
LenaBultmann <lena.bultmann@gmx.de>
2024-02-23 16:22:12 +0100

    adding controllers to userRoute

--- routes/users.js
+++ routes/users.js
@@ -5,6 +5,8 @@ import {
   getUsers,
   updateUser,
   loginUser,
+  updateContacts,
+  updateEvents,
 } from "../controllers/users.js";
 import { authenticate } from "../middlewares/authenticate.js";
 
@@ -15,6 +17,8 @@ userRouter.post("/login", loginUser);
 userRouter.get("/user", authenticate, getUser);
 userRouter.get("/all", getUsers);
 userRouter.put("/:id", updateUser);
+userRouter.put("/contacts/:id", updateContacts);
+userRouter.put("/events/:id", updateEvents);
 // userRouter.delete("/:id", deleteUser);
 
 
