ec997643343205e10b90801e2406a03e84957e60
Néstor Iriondo <nestoririondo@gmail.com>
2024-02-23 21:51:23 +0100

    check event exists middleware

--- controllers/todos.js
+++ controllers/todos.js
@@ -1,22 +1,17 @@
 import Event from "../models/Event.js";
 
 export const addAndAssignTodo = async (req, res) => {
-    const { id } = req.params;
     const { title, description, assignee } = req.body;
     const userId = req.user.id;
     try {
-      const event = await Event.findById(id);
-      if (!event) {
-        return res.status(404).json({ error: "Event not found." });
-      }
-      if (event.owner.toString() !== userId) {
+      if (req.event.owner.toString() !== userId) {
         return res
           .status(403)
           .json({ error: "User not authorized to edit this event." });
       }
       const todo = { title, description, assignee: assignee || null };
-      event.todos.push(todo);
-      const updatedEvent = await event.save();
+      req.event.todos.push(todo);
+      const updatedEvent = await req.event.save();
       return res.status(200).json(updatedEvent);
     } catch (error) {
       return res.status(500).json({ error: error.message });
@@ -24,24 +19,17 @@ export const addAndAssignTodo = async (req, res) => {
   };
   
   export const checkUncheckTodo = async (req, res) => {
-    const { id, todoId } = req.params;
+    const { todoId } = req.params;
     const userId = req.user.id;
     try {
-      const event = await Event.findById(id);
-      console.log(event.owner.toString(), "owner")
-      console.log(userId, "userId")
-      if (!event) {
-        return res.status(404).json({ error: "Event not found." });
-      }
-      if (event.owner.toString() !== userId && event.todos.id(todoId).assignee?.toString() !== userId) {
-        console.log(event.owner)
+      if (req.event.owner.toString() !== userId && req.event.todos.id(todoId).assignee?.toString() !== userId) {
         return res
           .status(403)
           .json({ error: "User not authorized to edit this event." });
       }
-      const todo = event.todos.id(todoId);
+      const todo = req.event.todos.id(todoId);
       todo.done = !todo.done;
-      const updatedEvent = await event.save();
+      const updatedEvent = await req.event.save();
       return res.status(200).json(updatedEvent);
     } catch (error) {
       return res.status(500).json({ error: error.message });
@@ -49,23 +37,19 @@ export const addAndAssignTodo = async (req, res) => {
   };
   
   export const editTodo = async (req, res) => {
-    const { id, todoId } = req.params;
+    const { todoId } = req.params;
     const { title, assignee } = req.body;
     const userId = req.user.id;
     try {
-      const event = await Event.findById(id)
-      if (!event) {
-        return res.status(404).json({ error: "Event not found." });
-      }
-      if (event.owner.toString() !== userId) {
+      if (req.event.owner.toString() !== userId) {
         return res
           .status(403)
           .json({ error: "User not authorized to edit this event." });
       }
-      const todo = event.todos.id(todoId);
+      const todo = req.event.todos.id(todoId);
       todo.title = title;
       todo.assignee = assignee;
-      const updatedEvent = await event.save();
+      const updatedEvent = await req.event.save();
       return res.status(200).json(updatedEvent);
     }
     catch (error) {
@@ -74,22 +58,18 @@ export const addAndAssignTodo = async (req, res) => {
   }
 
 export const deleteTodo = async (req, res) => {
-    const { id, todoId } = req.params;
+    const { todoId } = req.params;
     const userId = req.user.id;
     try {
-        const event = await Event.findById(id);
-        if (!event) {
-            return res.status(404).json({ error: "Event not found." });
-        }
-        if (event.owner.toString() !== userId) {
+        if (req.event.owner.toString() !== userId) {
             return res.status(403).json({ error: "User not authorized to edit this event." });
         }
-        const todo = event.todos.id(todoId);
+        const todo = req.event.todos.id(todoId);
         if (!todo) {
             return res.status(404).json({ error: "Todo not found." });
         }
-        event.todos.pull(todoId);
-        const updatedEvent = await event.save();
+        req.event.todos.pull(todoId);
+        const updatedEvent = await req.event.save();
         return res.status(200).json(updatedEvent);
     } catch (error) {
         return res.status(500).json({ error: error.message });
--- routes/events.js
+++ routes/events.js
@@ -2,6 +2,7 @@ import express from 'express';
 import { createEvent, getEvent, updateEvent, updateParticipants } from '../controllers/events.js';
 import { addAndAssignTodo, checkUncheckTodo, editTodo, deleteTodo } from '../controllers/todos.js';
 import { authenticate } from '../middlewares/authenticate.js';
+import { checkEventExists } from '../middlewares/events.js';
 
 const eventRouter = express.Router();
 
@@ -11,9 +12,9 @@ eventRouter.put('/:id', authenticate, updateEvent);
 eventRouter.put('/:id/participants', authenticate, updateParticipants);
 // eventRouter.delete('/:id', deleteEvent);
 
-eventRouter.post('/:id/todos/add', authenticate, addAndAssignTodo);
-eventRouter.put('/:id/todos/:todoId/toggle', authenticate, checkUncheckTodo);
-eventRouter.put('/:id/todos/:todoId/edit', authenticate, editTodo);
-eventRouter.delete('/:id/todos/:todoId', authenticate, deleteTodo);
+eventRouter.post('/:id/todos/add', authenticate, checkEventExists, addAndAssignTodo);
+eventRouter.put('/:id/todos/:todoId/toggle', authenticate, checkEventExists, checkUncheckTodo);
+eventRouter.put('/:id/todos/:todoId/edit', authenticate, checkEventExists, editTodo);
+eventRouter.delete('/:id/todos/:todoId', authenticate, checkEventExists, deleteTodo);
 
 export default eventRouter;
