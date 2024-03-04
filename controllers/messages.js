59ae66990d34fa02f31911bed23b21db7c0bf53a
Néstor Iriondo <nestoririondo@gmail.com>
2024-03-04 12:28:18 +0100

    upload to cloudinary

--- controllers/messages.js
+++ controllers/messages.js
@@ -1,4 +1,5 @@
 import Message from "../models/Message.js";
+import cloudinary from "../db/cloudinary.js";
 
 export const getMessages = async (req, res) => {
   const { id } = req.params;
@@ -22,10 +23,31 @@ export const postMessage = async (req, res) => {
   const { id } = req.params;
   const { id: sender } = req.user;
   try {
+    let imageUrl = '';
+    if (req.file) {
+      const result = await new Promise((resolve, reject) => {
+        const uploadStream = cloudinary.uploader.upload_stream(
+          {
+            folder: "messages",
+          },
+          (error, result) => {
+            if (error) reject(error);
+            else resolve(result);
+          }
+        );
+        // The file's data is sent to Cloudinary. req.file.buffer will contain the file's data as a binary buffer, which is what you're uploading.
+        //The .end() method on the upload stream is used to write this buffer to the stream, initiating the upload process.
+        uploadStream.end(req.file.buffer);
+      });
+      // Once the promise resolves, the result object contains details about the uploaded file, including its URL on Cloudinary's servers. After the upload is successful, we store the image URL returned by Cloudinary
+      imageUrl = result.url;
+    }
+
     const message = await Message.create({
       event: id,
       sender,
-      message: req.body.message.message,
+      text: req.body.text,
+      file: req.file ? imageUrl : null,
       readBy: [sender],
     });
     const messageWithSender = await message.populate({
--- routes/messages.js
+++ routes/messages.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import { getMessages, postMessage, getUnreadMessagesNumber, markMessagesAsRead } from '../controllers/messages.js';
 import { authenticate } from '../middlewares/authenticate.js';
+import { upload } from '../middlewares/upload.js';
 
 const messageRouter = express.Router();
 
 messageRouter.get('/:id', authenticate, getMessages);
-messageRouter.post('/:id', authenticate, postMessage);
+messageRouter.post('/:id', authenticate, upload.single('file'), postMessage);
 messageRouter.get('/:id/unread', authenticate, getUnreadMessagesNumber);
 messageRouter.put('/:id/read', authenticate, markMessagesAsRead);
 
